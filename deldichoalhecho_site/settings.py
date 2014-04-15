b45a5fb90fe65f706c3d26f47984b48dfa149191
Felipe Álvarez <falvarez@votainteligente.cl>
2014-04-15 12:13:37 -0300

    Updating settings for non heroku environments

--- deldichoalhecho_site/settings.py
+++ deldichoalhecho_site/settings.py
@@ -90,10 +90,10 @@ STATIC_URL = '/static/'
 
 #HEROKU SPECIFICS
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
 
-print DATABASES
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
