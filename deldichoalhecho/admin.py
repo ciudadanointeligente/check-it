ba946589ef471a4a2f6d0b041ac8ce0c2122dd32
Felipe Álvarez <luisfelipealvarez@gmail.com>
2014-04-16 00:11:59 -0300

    Adding a fulfillment as an inline in the promise admin

--- deldichoalhecho/admin.py
+++ deldichoalhecho/admin.py
@@ -3,15 +3,19 @@ from .models import Promise, Fulfillment, InformationSource, Category
 from popolo.models import Person
 
 # Register your models here.
-class PromiseAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Promise, PromiseAdmin)
 
 class FulfillmentAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Fulfillment, FulfillmentAdmin)
+
+class FulfillmentInlineAdmin(admin.StackedInline):
+	model = Fulfillment
+
+class PromiseAdmin(admin.ModelAdmin):
+    inlines = (FulfillmentInlineAdmin, )
+
+admin.site.register(Promise, PromiseAdmin)
 
 class InformationSourceAdmin(admin.ModelAdmin):
     pass
