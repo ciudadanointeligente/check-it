d251550f4e81c7d45082a620288a796321828224
Felipe Álvarez <falvarez@votainteligente.cl>
2014-04-24 10:55:31 -0300

    Moving assets because the previous config was not working

--- deldichoalhecho_site/settings.py
+++ deldichoalhecho_site/settings.py
@@ -130,8 +130,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'deldichoalhecho_web', 'static'),
-        os.path.join(BASE_DIR,' deldichoalhecho_theme', 'static'),
+        os.path.join(BASE_DIR, 'static'),
 )
 
 # END OF HEROKU SPECIFICS
