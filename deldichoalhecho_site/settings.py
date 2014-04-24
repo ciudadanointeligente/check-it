e2658deb138c535d4eddd4b44ef46ebf5cd1f84d
Felipe Álvarez <falvarez@votainteligente.cl>
2014-04-24 11:10:42 -0300

    separating assets in different apps

--- deldichoalhecho_site/settings.py
+++ deldichoalhecho_site/settings.py
@@ -129,8 +129,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(BASE_DIR, '..', 'deldichoalhecho_web', 'static'),
+        os.path.join(BASE_DIR, '..', ' deldichoalhecho_theme', 'static'),
 )
 
 # END OF HEROKU SPECIFICS
