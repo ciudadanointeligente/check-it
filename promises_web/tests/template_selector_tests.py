8bdf652feb87b65f47abfdd3b89405a3b1f5fccc
Felipe Álvarez <falvarez@votainteligente.cl>
2014-05-15 19:10:52 -0400

    Selecting themes but it is currently broken

--- promises_web/tests/template_selector_tests.py
+++ promises_web/tests/template_selector_tests.py
@@ -8,6 +8,7 @@ from django.core.urlresolvers import reverse
 from django.test import Client
 from taggit.models import Tag
 from constance import config
+from django.contrib.staticfiles import finders
 
 nownow = now()
 
@@ -22,4 +23,20 @@ class TemplateSelectorTestCase(TestCase):
         c = Client()
         response = c.get(url)
         self.assertTemplateUsed(response, 'base.html')
+
+    def atest_get_base_if_template_not_in_theme(self):
+    	'''If a certain template cannot be found uses the one in base'''
+    	config.CURRENT_THEME = 'test'
+    	url = reverse('promises_home')
+        c = Client()
+        response = c.get(url)
+        # template = response.resolve_template(response.template_name)
+        # print template
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.rendered_content, "testest")
+
+    def test_static_finder(self):
+    	'''Gets correctly the static files'''
+    	result = finders.find('images/favicon.ico')
+    	self.assertIsNotNone(result)
 
