695da5a9b3675815bb24e8904b35ded3c37c3e34
Felipe Álvarez <falvarez@votainteligente.cl>
2014-05-28 16:55:02 -0400

    Updating wsgi file to use project_site.settings and removing unnecesary fulfilments in example_data.json

--- project_site/wsgi.py
+++ project_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for deldichoalhecho_site project.
+WSGI config for Check-it project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deldichoalhecho_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_site.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
