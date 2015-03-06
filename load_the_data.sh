768ae562cbad3baf86bd2c6efe702bad43540896
Felipe Álvarez <falvarez@votainteligente.cl>
2015-03-06 20:17:25 -0300

    Adding a dataloader

---
+++ load_the_data.sh
@@ -0,0 +1,6 @@
+./manage.py loaddata edicion2015_data/popolo.json
+./manage.py loaddata edicion2015_data/categories.json
+./manage.py loaddata edicion2015_data/promises.json
+./manage.py loaddata edicion2015_data/information_sources.json
+./manage.py loaddata edicion2015_data/verificationdocuments.json
+./manage.py loaddata edicion2015_data/fulfillments.json
